25b6c45c6d45c2779b1f1ad59838a4a83a88fc32
mozochek <kirill.fockin2012@yandex.ru>
2020-04-03 11:26:46 +0300

    Небольшие изменения репозиториев

--- src/main/java/com/mozochek/repository/SportDisciplineRepository.java
+++ src/main/java/com/mozochek/repository/SportDisciplineRepository.java
@@ -1,7 +1,6 @@
 package com.mozochek.repository;
 
 import com.mozochek.entity.SportDiscipline;
-import com.mozochek.entity.SportKind;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SportDisciplineRepository extends CrudRepository<SportDiscipline, Integer> {
--- src/main/java/com/mozochek/repository/SportKindRepository.java
+++ src/main/java/com/mozochek/repository/SportKindRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface SportKindRepository extends CrudRepository<SportKind, Integer> {
 
     SportKind findByName(String name);
+
     SportKind findByNameAndCode(String name, String code);
 }
