aa12ef48ddc05085236d5da07c96a1bd34808f0f
mozochek <kirill.fockin2012@yandex.ru>
2020-05-15 12:37:27 +0300

    Финальная версия

--- backup/backup_db.bat
+++ backup/backup_db.bat
@@ -4,11 +4,14 @@ set year=%fulldate:~0,4%
 set month=%fulldate:~4,2%
 set day=%fulldate:~6,2%
 set foldername=%day%.%month%.%year%
+cd %~dp0%
+mkdir data
 cd %~dp0%data
 mkdir %foldername%
-cd %foldername%
-cd C:\Important\PostgreSQL\bin
 set p=%~dp0%data
+echo %p%
+cd C:\Important\PostgreSQL\bin
 pg_dump --dbname=postgresql://dbconnection:0000@127.0.0.1:5432/webdb --column-inserts --clean --file=%p%\%foldername%\webdb_backup.backup
 xcopy %p%\%foldername%\webdb_backup.backup %p%\last_backup\webdb_backup.backup* /Y
+pause
 exit
